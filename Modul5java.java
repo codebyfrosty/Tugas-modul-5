efc18ba2d12f08ec5a5360fdbcbaf43e96c9fa92
Mzbegs36 <63114734+Mzbegs36@users.noreply.github.com>
2020-04-26 12:59:51 +0700

    main class

---
+++ Modul5java.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modul5java;
+
+import java.util.Scanner;
+public class Modul5java {
+
+  
+    public static void main(String[] args) {
+       
+
+        String email, password;
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Email : ");
+        email = input.nextLine();
+        System.out.print("Password : ");
+        password = input.nextLine();
+        
+        userService satu = new userService( email, password );
+        satu.login();
+        
+    }
+    
+}
